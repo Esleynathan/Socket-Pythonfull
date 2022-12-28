f30c3baac00eee95140df293a10bb249ab6f5a06
Esley <esleynathan@hotmail.com>
2022-12-28 19:36:41 -0300

    Enviando Imagem Através da Rede - Socket

--- client.py
+++ client.py
@@ -3,13 +3,17 @@ import socket
 HOST = 'localhost'
 PORT = 8002
 
+arquivo = open('x.png', 'rb')
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.connect((HOST, PORT))
 
-sock.send(input('Digite sua mensagem: ').encode())
+## Solicitando uma mensagem para armazenar ##
+# sock.send(input('Digite sua mensagem: ').encode())
 
-sock.send(b"enviado2")
+sock.send(input('Digite o nome do arquivo: ').encode())
+sock.send(arquivo.read())
 
 confirmacao = sock.recv(1024)
 if confirmacao == b'ok':
--- server.py
+++ server.py
@@ -8,7 +8,17 @@ sock.bind ((HOST, PORT))
 sock.listen(5)
 
 while True:
+    ### Enviando uma mensagem ###
+    # novoSock, _ = sock.accept()
+    # mensagem = novoSock.recv(1024).decode() - Decodificando(desfazendo o binario) da mensagem.
+    # print(mensagem)
+    # novoSock.send(b'ok')
+
     novoSock, _ = sock.accept()
-    mensagem = novoSock.recv(1024).decode()
-    print(mensagem)
+    nomeArquivo = novoSock.recv(1024).decode()
+    novoArquivo = novoSock.recv(10000000)
+
+    with open (f'arquivos/{nomeArquivo}.png', 'wb') as arq:
+        arq.write(novoArquivo)
+
     novoSock.send(b'ok')
